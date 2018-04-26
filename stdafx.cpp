7bc1ea0bbba90e32ec30b9bfb47ce705dff01b38
parallelProject <qianjiaowu@whu.edu.cn>
2018-04-26 10:00:30 +0800

    Add files via upload

---
+++ stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : only contains the source files containing the standard inclusion file
+// points.pch will be the precompiled header
+// stdafx.obj contains the type information of the precompiled header
+
+#include "stdafx.h"
+
+// TODO: included in the STDAFX.H
+// Refer to any required additional header files, instead of referencing them in this file
