2af946170253b4afbccda596639f896d5a856172
parallelProject <qianjiaowu@whu.edu.cn>
2018-04-26 10:01:52 +0800

    Add files via upload

---
+++ targetver.h
@@ -0,0 +1,7 @@
+#pragma once
+
+//Include the SDKDDKVer.h define the highest available version of Windows platform
+
+//If you want to generate applications for the previous Windows platform, include winsdkver.h
+//The WIN32_WINNT macro is set to the platform to support and then includes the sdkddkver.h.
+#include <SDKDDKVer.h>
